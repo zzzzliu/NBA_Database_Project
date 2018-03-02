331c8a7aa637daa10e2819f77d7354fce39d825a
Hongzhi Liu <liu.hongz@husky.neu.edu>
2018-03-02 01:11:57 -0500

    remove useless data

--- playerStatisticsAGame.sql
+++
@@ -1,4 +0,0 @@
-select t.name, g.gameDate, p.win, p.home, ts.*
-from Team t, Game g, Participation p, TeamStatisticsAGame ts
-where t.id = p.isParticipatedBy and p.participates = g.id and t.name = "Atlanta Hawks"
-and ts.partOfTeam = p.isParticipatedBy and ts.partOfGame = p.participates
